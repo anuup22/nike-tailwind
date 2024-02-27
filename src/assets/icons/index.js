03a2bd2c8b406581940f2a582f771ef500dd8589
Anup <anuup22@outlook.com>
2024-02-28 00:43:30 +0530

    shoe section completed

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,7 +3,7 @@ import chevronLeft from './chevron-left.svg'
 import cheveronRight from './chevron-right.svg'
 import copyrightSign from './copyright-sign.svg'
 import facebook from './facebook.svg'
-import hamburger from './hamburger.svg'
+import menuBar from './menuBar.svg'
 import instagram from './instagram.svg'
 import support from './support.svg'
 import shieldTick from './shield-tick.svg'
@@ -17,7 +17,7 @@ export {
     chevronLeft,
     copyrightSign,
     facebook,
-    hamburger,
+    menuBar,
     instagram,
     support,
     shieldTick,
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,66 @@
+import { useState } from "react"
+import { arrowRight } from "../assets/icons"
+import { bigShoe1 } from "../assets/images"
+import Button from "../components/Button"
+import ShoeCard from "../components/ShoeCard"
+import { shoes, statistics } from "../constants"
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
+
   return (
-    <div>Hero</div>
+    <section 
+      id="home" 
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        {/* Title */}
+        <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The new Arrival</span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish and comfortable shoes for all occasions. Quality and comfort are guaranteed. Inovation for your active lifestyle.
+        </p>
+        {/* Shop now button */}
+        <Button label="Shop now" iconURL={arrowRight}/>
+
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {/* Statistics */}
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        {/* Big shoe image */}
+        <img 
+          src={bigShoeImg} 
+          alt="ShoeCollection" 
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+            {/* Shoe cards */}
+            {shoes.map((shoe) => (
+              <div key={shoe}>
+                <ShoeCard
+                  imgURL={shoe}
+                  changeBigShoeImg={(shoe) => {setBigShoeImg(shoe)}}
+                  bigShoeImg={bigShoeImg}
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
